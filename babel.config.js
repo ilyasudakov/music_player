de420710bd2ebc23be2a392a833c5449e1c46107
ilyasudakov <ilyasudakov@inbox.ru>
2020-07-22 16:41:43 +0300

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
   presets: ['@vue/cli-plugin-babel/preset'],
 }
